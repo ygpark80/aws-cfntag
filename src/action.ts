aa108c21fc932de3b3c4c750e7ccaced242df4a2
YG Park <ygpark80@gmail.com>
2023-11-26 21:09:34 +0900

    v1.0.9 (#3)

--- src/action.ts
+++ src/action.ts
@@ -4,17 +4,21 @@ import { parseTags } from "./utils"
 
 async function run() {
     const stackName = core.getInput("stack-name")
+    const profile = core.getInput("profile")
     const tags = core.getInput("tags")
 
+    if (profile) process.env.AWS_PROFILE = profile
+
     console.log("🏷️ Tagging CloudFormation stack...")
-    console.log(`  📋 Stack name: ${stackName}`)
-    console.log(`  🏷️ Tags:`, parseTags(tags))
+    console.log(`📋 Stack name: ${stackName}`)
+    console.log(`🏷️ Tags:`, parseTags(tags))
 
     await tagStack(stackName, parseTags(tags))
 }
 
 if (require.main === module) {
     run().catch((error) => {
+        console.error(error)
         core.setFailed(error)
     })
 }
