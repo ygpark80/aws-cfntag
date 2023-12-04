7ca9b634d776e452b8b9c9d97f750089345bd57e
YG Park <ygpark80@gmail.com>
2023-12-04 14:51:04 +0900

    v1.0.12

--- src/aws/ecs.ts
+++ src/aws/ecs.ts
@@ -1,7 +1,7 @@
 import { ResourceTagger, TagResourceResult } from "."
 import utils from "./utils"
 import { StackResource } from "@aws-sdk/client-cloudformation"
-import { ECSClient, TagResourceCommand } from "@aws-sdk/client-ecs"
+import { ECSClient, TagResourceCommand, UpdateServiceCommand, ListTasksCommand } from "@aws-sdk/client-ecs"
 
 export default class ECS implements ResourceTagger {
 
@@ -9,23 +9,43 @@ export default class ECS implements ResourceTagger {
         const region = utils.region(resource)
         const accountId = utils.accountId(resource)
         const ecs = new ECSClient()
+        const _tags = Object.entries(tags).map(([key, value]) => ({ key, value }))
 
         switch (resource.ResourceType) {
             case "AWS::ECS::Cluster":
                 try {
                     const resourceArn = `arn:aws:ecs:${region}:${accountId}:cluster/${resource.PhysicalResourceId!}`
-                    const _tags = Object.entries(tags).map(([key, value]) => ({ key, value }))
                     await ecs.send(new TagResourceCommand({ resourceArn, tags: _tags }))
                     return TagResourceResult.Success
                 } catch (error) {
                     throw error
                 }
             case "AWS::ECS::TaskDefinition":
-            case "AWS::ECS::Service":
                 try {
                     const resourceArn = resource.PhysicalResourceId
-                    const _tags = Object.entries(tags).map(([key, value]) => ({ key, value }))
                     await ecs.send(new TagResourceCommand({ resourceArn, tags: _tags }))
+                    return TagResourceResult.Success
+                } catch (error) {
+                    throw error
+                }
+            case "AWS::ECS::Service":
+                try {
+                    const promises: Promise<unknown>[] = []
+                    const resourceArn = resource.PhysicalResourceId
+                    promises.push(ecs.send(new TagResourceCommand({ resourceArn, tags: _tags })))
+                    promises.push(ecs.send(new UpdateServiceCommand({
+                        service: resource.PhysicalResourceId,
+                        propagateTags: "SERVICE"
+                    })))
+
+                    const { taskArns } = await ecs.send(new ListTasksCommand({ serviceName: resource.PhysicalResourceId, }))
+                    if (taskArns) {
+                        promises.push(...taskArns.map((resourceArn) => {
+                            return ecs.send(new TagResourceCommand({ resourceArn, tags: _tags }))
+                        }))
+                    }
+                    await Promise.all(promises)
+
                     return TagResourceResult.Success
                 } catch (error) {
                     throw error
